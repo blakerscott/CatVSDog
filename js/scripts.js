6bc61ff5b18bfaf512e739d20b35127b0d92d092
Blake Scott and Chris Young <ctopheryoung@gmail.com>
2016-01-12 10:37:27 -0800

    add images to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-sdf 
+$(document).ready(function() {
+  $("button#bark").click(function() {
+
+    $("div#dog").before("<img src=img/woof.png>");
+    });
+
+  $("button#meow").click(function() {
+    $("div#cat").after("<img src=img/meow2.png>");
+
+    });
+
+
+})
