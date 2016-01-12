277e1c46a41889643f00e4aaed0df2bb144fb761
Blake Scott and Chris Young <blakerscott@gmail.com>
2016-01-12 11:00:00 -0800

    add css styles, js to remove

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   $("button#bark").click(function() {
-
-    $("div#dog").before("<img src=img/woof.png>");
+    $("div#dog").prepend("<img src=img/woof.png>");
+    $("div#dog").children("img").first().click(function() {
+      $(this).remove();
     });
+  });
 
   $("button#meow").click(function() {
-    $("div#cat").after("<img src=img/meow2.png>");
-
+    $("div#cat").prepend("<img src=img/meow2.png>");
+    $("div#cat").children("img").first().click(function() {
+      $(this).remove();
     });
+  });
 
 
 })
